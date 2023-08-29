98797590f08d0d823a01daf265025e2efdfcaa10
RodolfoLobos <stevenlovoz3@gmail.com>
2023-08-28 21:00:51 -0600

    Ejercicios

---
+++ Ejercicios/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Ejercicios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(138, 150);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(140, 40);
+            this.btnProcesar.TabIndex = 0;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEdad.Location = new System.Drawing.Point(47, 37);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(76, 27);
+            this.lblEdad.TabIndex = 1;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEdad.Location = new System.Drawing.Point(129, 34);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(165, 35);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 450);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.btnProcesar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox txtEdad;
+    }
+}
+
---
+++ Ejercicios/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            int edad = int.Parse(txtEdad.Text);
+            /* if (edad <= 2)
+            {
+                MessageBox.Show("Eres un bebe");
+            }else if (edad < 12)
+            {
+                MessageBox.Show("Eres un niño");
+            }
+            else if (edad < 18){
+                MessageBox.Show("Eres un adolescente");
+            }else if (edad <= 65){
+                MessageBox.Show("Eres mayor de edad");
+            }else if (edad <= 80){
+                MessageBox.Show("Adulto Mayor");
+            }else{
+                MessageBox.Show("Larga vida...");
+            }/
+
+            /**
+            *if ternario
+            */
+            /*String msg = edad <= 2 ? "Eres un bebé" :
+            edad < 12 ? "Eres un niño" :
+            edad < 18 ? "" :
+            edad <= 65 ? "" :
+            edad <= 80 ? "Eres un adulto mayor" : "Larga vida...";*/
+
+            /**
+             * switch
+             */
+            String msg;
+            switch(edad){
+                case 2:
+                    msg = "Tienes 2 años";
+                    break;
+
+                case 12:
+                    msg = "Tienes 12 años";
+                    break;
+
+                default:
+                    msg = "Otra edad";
+                    break;
+            }
+            MessageBox.Show(msg);
+
+
+        } 
+    }
+}
